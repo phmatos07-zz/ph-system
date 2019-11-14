09f8feec89431010fd4773572c1e1825949f5b0c
Phelipe Matos <phelipe.matos@viavarejo.com.br>
2019-11-14 17:39:04 -0300

    Limpeza do 'app.component.ts' e 'app.component.html'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ProxifyService } from './../../projects/ph-core-utils/src/lib/services/proxify/proxify.service';
-import { StorageTypeEnum } from './../../projects/ph-core-utils/src/lib/services/storage/enum/storage-type.enum';
 import { StorageService } from './../../projects/ph-core-utils/src/lib/services/storage/storage.service';
 
 @Component({
@@ -10,8 +9,6 @@ import { StorageService } from './../../projects/ph-core-utils/src/lib/services/
 })
 export class AppComponent implements OnInit {
 
-  objeto = {};
-
   constructor(
     private storageService: StorageService,
     private proxifyService: ProxifyService
@@ -19,29 +16,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.initEventListener();
-
-    setTimeout(() => {
-      this.storageService.clearStorage();
-    }, 5000);
   }
 
   private initEventListener(): void {
-
-    this.storageService.keyName = 'teste';
-    this.objeto = {
-      nome: 'phelipe',
-      idade: 31
-    };
-    this.storageService.setItem(this.objeto, StorageTypeEnum.SESSION_STORAGE);
-  }
-
-  get nome(): string {
-    const sessionStorage = this.storageService.getItem(StorageTypeEnum.SESSION_STORAGE);
-    return this.proxifyService.proxy<string>(sessionStorage, 'nome');
-  }
-
-  get idade(): string {
-    const sessionStorage = this.storageService.getItem(StorageTypeEnum.SESSION_STORAGE);
-    return this.proxifyService.proxy<string>(sessionStorage, 'idade');
   }
 }
