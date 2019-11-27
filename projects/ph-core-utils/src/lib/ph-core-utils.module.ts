db1557492bb2c0989fab5ad8952456aa3cd775d3
Phelipe Matos <phelipe.matos@viavarejo.com.br>
2019-11-27 12:42:04 -0300

    Implementação da directive ‘InputMaskDirective’.

--- projects/ph-core-utils/src/lib/ph-core-utils.module.ts
+++ projects/ph-core-utils/src/lib/ph-core-utils.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from './components/components.module';
+import { DirectivesModule } from './directives/directives.module';
 import { ServicesModule } from './services/services.module';
 
 @NgModule({
   imports: [
     ComponentsModule,
+    DirectivesModule,
     ServicesModule
   ],
   exports: [
-    ComponentsModule
+    ComponentsModule,
+    DirectivesModule
   ]
 })
 export class PhCoreUtilsModule { }
