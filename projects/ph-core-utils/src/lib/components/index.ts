236bc2693b57cd670df48d7d47d9ae9d6e58d918
PHMATOS07 <phelipe.matos07@gmail.com>
2020-02-22 17:51:39 -0300

    Criando componente de Autocomplete

--- projects/ph-core-utils/src/lib/components/index.ts
+++ projects/ph-core-utils/src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export * from './alert-message/alert-message.component';
+export * from './autocomplete/autocomplete.component';
 export * from './components.module';
 export * from './horizontal-timeline/horizontal-timeline.component';
